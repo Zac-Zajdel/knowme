5b19ac40db9a5713b215f82727e897f89ebab2f6
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2019-02-11 17:49:07 -0500

    Multiple changes

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,26 +1,58 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
 import './Settings.css';
 
-const particlesOptions = {
-  particles: {
-    number: {
-      value: 100,
-      density: {
-        enable: true,
-        value_area: 600
-      }
-    }
-  }
-};
+
 
 class Settings extends Component {
+
+
+
   render() {
     return (
-      <div className='page'>
-        <Particles className='particles'
-          params={particlesOptions}
-        />
+      <div className='wrapper'>
+
+
+        <div className='container'>
+          <form>
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input type="email" className="form-control" id="inputEmail4" placeholder="Email" />
+              </div>
+              <div className="form-group col-md-6">
+                <label for="inputPassword4">Password</label>
+                <input type="password" className="form-control" id="inputPassword4" placeholder="Password" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label for="inputAddress">Address</label>
+              <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+            </div>
+            <div className="form-group">
+              <label for="inputAddress2">Address 2</label>
+              <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+            </div>
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label for="inputCity">City</label>
+                <input type="text" className="form-control" id="inputCity" />
+              </div>
+              <div className="form-group col-md-4">
+                <label for="inputState">State</label>
+                <select id="inputState" className="form-control">
+                  <option selected>Choose...</option>
+                  <option>...</option>
+                </select>
+              </div>
+              <div className="form-group col-md-2">
+                <label for="inputZip">Zip</label>
+                <input type="text" className="form-control" id="inputZip" />
+              </div>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Sign in</button>
+          </form>
+        </div>
       </div>
     );
   }
